7c83b66221e4eede977a7bfac5ead629972e7fa4
John Graham <johnegraham2@gmail.com>
2023-03-02 14:38:28 -0500

    started work on board layout

--- src/js/Level.js
+++ src/js/Level.js
@@ -3,20 +3,53 @@ import * as THREE from 'three';
 export default class Level {
   constructor(screen) {
     this.screen = screen;
-    this.cube;
+    this.cube_size = 1;
+    this.board_rows = 7;
 
-    this.createCube();
+    this.createBoard();
   }
 
-  createCube() {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x22dddd });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.screen.scene.add(this.cube);
+  createBoard() {
+    const colors = [
+      0x22dddd,
+      0x62dddd,
+      0x922dddd,
+      0xf2dddd,
+      0x221ddd,
+      0x544d33,
+      0x227d74,
+      0x22dddd,
+      0x22dd3d,
+      0x22dd8d
+    ];
+
+    const geometry = new THREE.BoxGeometry(this.cube_size, this.cube_size, this.cube_size);
+    const cube_hypot = Math.hypot(this.cube_size, this.cube_size);
+    
+    for(let row_count = this.board_rows; row_count > 0; row_count--) {
+      const y_pos = (this.board_rows - row_count) * this.cube_size;
+      const z_pos = 0 - (this.board_rows - row_count) * (cube_hypot / 2);
+      console.log(y_pos);
+      
+      for(let cube_count = row_count; cube_count > 0; cube_count--) {
+        const material = new THREE.MeshLambertMaterial({ color: colors[Math.floor(Math.random() * (9 - 0 + 1) + 0)]});
+        const start_x_offset = 0 - (cube_hypot * cube_count / 2);
+        const x_pos = start_x_offset + ((row_count - cube_count) * (cube_hypot / 2));
+        let cube = new THREE.Mesh(geometry, material);
+        
+        cube.position.x = x_pos;
+        cube.position.y = y_pos;
+        cube.position.z = z_pos;
+  
+        cube.rotation.y = 0.785398;
+        
+        this.screen.scene.add(cube);
+
+      }
+    }
   }
 
   updateCube() {
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+
   }
 }
--- src/js/Screen.js
+++ src/js/Screen.js
@@ -12,13 +12,24 @@ export default class Screen {
 
   initThree() {
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    var ambient_light = new THREE.AmbientLight(0x404040);
+    this.scene.add(ambient_light);
+
+    let light = new THREE.HemisphereLight(0x404040, 0xFFFFFF, 0.5);
+    this.scene.add(light);
+
+    let direction_light = new THREE.DirectionalLight(0x404040, 0.5);
+    this.scene.add(direction_light);
 
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
-    this.camera.position.z = 5;
+    this.camera.position.z = 7;
+    this.camera.position.y = 11;
+    this.camera.rotation.x= -0.685398;
   }
 
   renderScene() {
