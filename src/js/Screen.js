936ab308da89e4e86eba5f09e259f742193450b0
John Graham <johnegraham2@gmail.com>
2023-03-02 19:19:52 -0500

    tweaks to board and camera layout

--- src/js/Screen.js
+++ src/js/Screen.js
@@ -12,7 +12,7 @@ export default class Screen {
 
   initThree() {
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     var ambient_light = new THREE.AmbientLight(0x404040);
     this.scene.add(ambient_light);
@@ -27,9 +27,10 @@ export default class Screen {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
-    this.camera.position.z = 7;
-    this.camera.position.y = 11;
-    this.camera.rotation.x= -0.685398;
+    this.camera.position.x = -.5;
+    this.camera.position.z = 15;
+    this.camera.position.y = 15.5;
+    this.camera.rotation.x= -0.58398;
   }
 
   renderScene() {
